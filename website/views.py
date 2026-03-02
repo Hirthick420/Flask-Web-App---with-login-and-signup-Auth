3680137bf00e13abd78cc2693841118c2d9b7358
Hirthick420 <hirthickshreenath0420@gmail.com>
2026-03-01 20:15:40 -0500

    Create base Jinja template and extend it across home/login/signup pages

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 #This file has all the url/ this file is blueprint of our project
-from flask import Blueprint 
+from flask import Blueprint, render_template
 
 views = Blueprint('views',__name__)
 
 @views.route('/') 
 def home():
-    return "<h1> Hello there!! <h1>"
+    return render_template("home.html")

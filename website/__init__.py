caf001b2eed82c4f15738f7ab159179755f9dad3
Hirthick420 <hirthickshreenath0420@gmail.com>
2026-03-03 22:41:50 -0500

    Set up SQLAlchemy bootstrap and User/Note models with FK relationship

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,16 @@
 #This file will make website folder a python package
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app(): #creating a function
     app = Flask (__name__)
     app.config['SECRET_KEY'] = 'hirthick123' #Never share this secret key for developing purpose
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     #This is the connection part of auth.py and views.py
     from .auth import auth #from the auth file import auth which is the blueprint
@@ -17,6 +24,16 @@ def create_app(): #creating a function
     
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/') 
+
+    from .models import User, Note
+    create_database(app) #This a database creation function
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print('Created Database!')
 
 
